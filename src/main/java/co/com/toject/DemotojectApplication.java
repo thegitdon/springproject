4be3c0a54ec3b81c1b88d7018c08a6833c837f53
Ronald Girón <ronald.giron@olsoftware.com>
2022-01-14 11:21:08 -0500

    Spring Quickstart

---
+++ src/main/java/co/com/toject/DemotojectApplication.java
@@ -0,0 +1,13 @@
+package co.com.toject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemotojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemotojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/toject/DemotojectApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.toject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemotojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
